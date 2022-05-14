ff150e7bdd660db64a6dc4db96c18d5774871bf0
gsanchit01 <sanchitgupta0101@gmail.com>
2022-05-14 08:47:57 +0530

    Updates

---
+++ precache-manifest.01a1dabd82d78d5b3d9ba5690c787e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ad0d1973c77e295a86",
+    "url": "/Archit_portfolio/static/css/main.58d7779d.chunk.css"
+  },
+  {
+    "revision": "48ad0d1973c77e295a86",
+    "url": "/Archit_portfolio/static/js/main.48ad0d19.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Archit_portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4f084981337ea4118b5a",
+    "url": "/Archit_portfolio/static/js/runtime~main.4f084981.js"
+  },
+  {
+    "revision": "081edd9f7434d1be88cdab83c6d961be",
+    "url": "/Archit_portfolio/index.html"
+  }
+];
